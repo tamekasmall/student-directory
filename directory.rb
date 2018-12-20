25eefdcd24207b4c2f2242553b768b921f333644
Tameka <tamekasmall@hotmail.com>
2018-12-20 13:26:14 +0000

    student print using iteration

--- directory.rb
+++ directory.rb
@@ -14,15 +14,7 @@ students = [
 ]
 puts "The students of Villains Academy"
 puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+puts "Overall, we have #{students.count} great students"
