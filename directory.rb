2913af2066243dc01e1503c4e2a7ce43ea6c0020
Tameka <tamekasmall@hotmail.com>
2018-12-22 15:58:05 +0000

    Exercise 8.7 Plural/singular student, cohort as userinput, converted to symbol

--- directory.rb
+++ directory.rb
@@ -1,113 +1,37 @@
 $students = []
 
 def input_students
-  puts "Please enter the name of the students"
+  puts "Please enter the name of the student"
   name = gets.chomp
+  puts "Please enter cohort"
+  cohort = gets.chomp
   puts "Please enter country of origin"
   country_origin = gets.chomp
-  puts "Please enter height (eg for 175cm)"
+  puts "Please enter height (eg '175' for 175cm)"
   height = gets.chomp
   puts "Please enter your favourite sport"
   sport = gets.chomp
-  puts "To finish, enter'q'or to add another student enter 'add'then double tap ENTER"
+
+  puts "1.)......TO ADD STUDENT 'add' then ENTER".rjust(30)
+  puts "2.)......TO FINISH 'q' then double tap ENTER".rjust(30)
   finish = gets.chomp
 
   while !name.empty? do
-    $students << {name: name, cohort: :november, country_origin: country_origin, height: height.to_i, sport: sport}
-    puts "Now we have #{$students.length} students"
-    name = gets.chomp
+    $students << {name: name, cohort: cohort.to_sym, country_origin: country_origin, height: height.to_i, sport: sport}
+      if $students.length < 2
+        puts "Now we have #{$students.length} student"
+        break
+      elsif $students.length >= 2
+        puts "Now we have #{$students.length} students"
+        name = gets.chomp
+      end
   end
-  if finish == "add"
-    input_students
-  else finish == "q"
+    if finish == "add"
+      input_students
+    else finish == "q"
     $students
+    end
   end
-end
-def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
-end
-def print(students)
-  index = 0
-  while index < $students.length
-    puts "#{index + 1}. #{$students[index][:name]} (#{$students[index][:cohort]} cohort)"
-    index += 1
-  end
-end
-def print_footer(names)
-  puts "Overall, we have #($students.length} great students"
-end
-
-students = input_students
-print_header
-print(students)$students = []
-
-def input_students
-  puts "Please enter the name of the students"
-  name = gets.chomp
-  puts "Please enter country of origin"
-  country_origin = gets.chomp
-  puts "Please enter height (eg for 175cm)"
-  height = gets.chomp
-  puts "Please enter your favourite sport"
-  sport = gets.chomp
-  puts "To finish, enter'q'or to add another student enter 'add'then double tap ENTER"
-  finish = gets.chomp
-
-  while !name.empty? do
-    $students << {name: name, cohort: :november, country_origin: country_origin, height: height.to_i, sport: sport}
-    puts "Now we have #{$students.length} students"
-    name = gets.chomp
-  end
-  if finish == "add"
-    input_students
-  else finish == "q"
-    $students
-  end
-end
-def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
-end
-def print(students)
-  index = 0
-  while index < $students.length
-    puts "#{index + 1}. #{$students[index][:name]} (#{$students[index][:cohort]} cohort)"
-    index += 1
-  end
-end
-def print_footer(names)
-  puts "Overall, we have #($students.length} great students"
-end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)$students = []
-
-def input_students
-  puts "Please enter the name of the students"
-  name = gets.chomp
-  puts "Please enter country of origin"
-  country_origin = gets.chomp
-  puts "Please enter height (eg for 175cm)"
-  height = gets.chomp
-  puts "Please enter your favourite sport"
-  sport = gets.chomp
-  puts "To finish, enter'q'or to add another student enter 'add'then double tap ENTER"
-  finish = gets.chomp
-
-  while !name.empty? do
-    $students << {name: name, cohort: :november, country_origin: country_origin, height: height.to_i, sport: sport}
-    puts "Now we have #{$students.length} students"
-    name = gets.chomp
-  end
-  if finish == "add"
-    input_students
-  else finish == "q"
-    $students
-  end
-end
 def print_header
   puts "The students of my cohort at Makers Academy".center(25)
   puts "--------------------------------".center(50)
@@ -120,11 +44,14 @@ def print(students)
   end
 end
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  if $students.length == 1
+    puts "Overall, we have #{$students.length} great student"
+  elsif $students.length > 1
+    puts "Overall, we have #{$students.length} great students"
+  end
 end
 
 students = input_students
 print_header
 print(students)
-print_footer(students)
 print_footer(students)
