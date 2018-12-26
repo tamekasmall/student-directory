1048d10906aa9f4e9f7d638e17b83d3599aca8c1
Tameka <tamekasmall@hotmail.com>
2018-12-26 17:14:01 +0000

    Exercise 8.8 printing students for given cohort

--- directory.rb
+++ directory.rb
@@ -1,15 +1,14 @@
 $students = []
-
 def input_students
   puts "Please enter the name of the student"
   name = gets.chomp
     if name == ""
-      name = "name: not entered"
+    name = "name: not entered"
     end
   puts "Please enter cohort"
   cohort = gets.chomp
     if cohort == ""
-      cohort = "not entered: "
+    cohort = "not entered: "
     end
   puts "Please enter country of origin"
   country_origin = gets.chomp
@@ -17,19 +16,17 @@ def input_students
   height = gets.chomp
   puts "Please enter your favourite sport"
   sport = gets.chomp
-
-  puts "1.)......TO ADD STUDENT 'add' then ENTER".rjust(30)
-  puts "2.)......TO FINISH 'q' then double tap ENTER".rjust(30)
+  puts "1.)......To ADD STUDENT 'add' then ENTER".rjust(30)
+  puts "2.)......To FINISH 'q' then double tap ENTER".rjust(30)
   finish = gets.chomp
-
   while !name.empty? do
-    $students << {name: name.to_sym, cohort: cohort.to_sym, country_origin: country_origin, height: height.to_i, sport: sport}
+    $students << {name: name.to_sym, cohort: cohort.to_sym, country_origin: country_origin, height: height, sport: sport}
       if $students.length < 2
         puts "Now we have #{$students.length} student"
-        break
+          break
       elsif $students.length >= 2
         puts "Now we have #{$students.length} students"
-        name = gets.chomp
+          name = gets.chomp
       end
   end
     if finish == "add"
@@ -37,17 +34,17 @@ def input_students
     else finish == "q"
       $students
     end
-  end
+end
 def print_header
   puts "The students of my cohort at Makers Academy".center(20)
   puts "--------------------------------".center(45)
 end
 def print(students)
   index = 0
-  while index < $students.length
-    puts "#{index + 1}. #{$students[index][:name]} (#{$students[index][:cohort]} cohort)"
-    index += 1
-  end
+    while index < $students.length
+      puts "#{index + 1}. #{$students[index][:name]} (#{$students[index][:cohort]} cohort)"
+        index += 1
+    end
 end
 def print_footer(names)
   if $students.length == 1
@@ -56,27 +53,39 @@ def print_footer(names)
     puts "Overall, we have #{$students.length} great students"
   end
 end
-def edit_info(students)
+def print_student_by_cohort(students)
+  array = []
+    puts "Enter cohort to print"
+      user_input = gets.chomp.downcase.to_sym
+      $students.map {|student| array << student if student[:cohort] == user_input}
+      array.each {|student| puts "#{student[:name]} (#{student[:cohort]})"}
+    end
+
+def option_menu(students) # Added a menu for better usability
   puts "  "
-  puts "To make any changes, enter 'c'"
+  puts "1.)......To make any CHANGES, ENTER 'c'"
+  puts "2.)......To SEARCH students by cohort ENTER 's'"
+  puts "3.)......To FINISH press ENTER"
   choice = gets.chomp.downcase
     if choice == "c"
       puts "Enter the student number you want to change"
-      student_num = gets.chomp.to_i
+        student_num = gets.chomp.to_i
       puts "Enter the correct name"
-      new_name = gets.chomp.to_sym
+        new_name = gets.chomp.to_sym
       puts "Enter the correct cohort"
-      new_cohort = gets.chomp.to_sym
-      index = (student_num - 1)
-      $students[index][:name] = new_name
-      $students[index][:cohort] = new_cohort
-      else
+        new_cohort = gets.chomp.to_sym
+        index = (student_num - 1)
+        $students[index][:name] = new_name
+        $students[index][:cohort] = new_cohort
+      end
+    if choice == "s"
+        print_student_by_cohort(students)
+    else
+        print(students)
     end
-
 end
 students = input_students
 print_header
 print(students)
 print_footer(students)
-edit_info(students)
-print(students)
+option_menu(students)
