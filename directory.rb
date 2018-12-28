251e56f20eda9437e929bcf6759951c3a8124389
Tameka <tamekasmall@hotmail.com>
2018-12-28 18:44:15 +0000

    Exercise 11: Saving the data to file

--- directory.rb
+++ directory.rb
@@ -36,6 +36,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 def show_students
@@ -49,6 +50,8 @@ def process(selection)
        input_students
      when "2"
        show_students
+     when "3"
+       save_students
      when "9"
        exit
      else
@@ -75,6 +78,15 @@ def print_footer #8.9 pluralisation for student/s
   elsif @students.length > 1
     puts "Overall, we have #{@students.count} great students"
   end
+end
+def save_students
+  file = File.open("student.csv", "w")
+  @students.each do |student|
+    students_data = [student[:name], student[:cohort]]
+    csv_line = students_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 interactive_menu
 
