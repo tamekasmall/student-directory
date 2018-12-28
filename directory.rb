2026dd1ddd5eaa3020f7acc2694543db1f29c970
Tameka <tamekasmall@hotmail.com>
2018-12-28 19:21:39 +0000

    Exercise 12: Loading the data from file

--- directory.rb
+++ directory.rb
@@ -37,6 +37,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from student.csv"
   puts "9. Exit"
 end
 def show_students
@@ -52,6 +53,8 @@ def process(selection)
        show_students
      when "3"
        save_students
+     when "4"
+       load_students
      when "9"
        exit
      else
@@ -85,6 +88,14 @@ def save_students
     students_data = [student[:name], student[:cohort]]
     csv_line = students_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+def load_students
+  file = File.open(".student.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students  << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
