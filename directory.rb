f708479e5c5a6dd698f10cec2371a45d7c02810c
Tameka <tamekasmall@hotmail.com>
2018-12-28 17:46:45 +0000

    Exercise 10: refactoring

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-$students = []
+@students = []
 def input_students
   puts "Please enter the name of the student"
   name = gets.chop
@@ -16,78 +16,94 @@ def input_students
   height = gets.chop
   puts "Please enter your favourite sport"
   sport = gets.chop
-  puts "1.)......To ADD STUDENT 'add' then ENTER".rjust(30)
-  puts "2.)......To FINISH 'q' then double tap ENTER".rjust(30)
-  finish = gets.chop
   while !name.empty? do
-    $students << {name: name.to_sym, cohort: cohort.to_sym, country_origin: country_origin, height: height, sport: sport}
-      if $students.length < 2
-        puts "Now we have #{$students.length} student"
+    @students << {name: name.to_sym, cohort: cohort.to_sym, country_origin: country_origin, height: height, sport: sport}
+      if @students.length < 2
+        puts "Now we have #{@students.count} student"
           break
-      elsif $students.length >= 2
-        puts "Now we have #{$students.length} students"
+      elsif @students.length >= 2
+        puts "Now we have #{@students.count} students"
           name = gets.chop
       end
   end
-    if finish == "add"
-      input_students
-    else finish == "q"
-      $students
-    end
 end
+def interactive_menu
+  loop do
+   print_menu
+    process(gets.chomp)
+  end
+end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+def process(selection)
+    case selection
+     when "1"
+       input_students
+     when "2"
+       show_students
+     when "9"
+       exit
+     else
+       puts "I don't know what you meant, try again"
+   end
+end
+
 def print_header
   puts "The students of my cohort at Makers Academy".center(20)
   puts "--------------------------------".center(45)
 end
-def print(students)
+def print_student_list
   index = 0
-    while index < $students.length
-      if $students.length >= 1
-      puts "#{index + 1}. #{$students[index][:name]} (#{$students[index][:cohort]} cohort)"
+    while index < @students.length
+      if @students.length >= 1
+      puts "#{index + 1}. #{@students[index][:name]} (#{@students[index][:cohort]} cohort)"
         index += 1
       end
-    end
+   end
 end
-def print_footer(names) #8.9 pluralisation for student/s
-  if $students.length == 1
-    puts "Overall, we have #{$students.length} great student"
-  elsif $students.length > 1
-    puts "Overall, we have #{$students.length} great students"
+def print_footer #8.9 pluralisation for student/s
+  if @students.length == 1
+    puts "Overall, we have #{@students.count} great student"
+  elsif @students.length > 1
+    puts "Overall, we have #{@students.count} great students"
   end
 end
-def print_student_by_cohort(students) #8.8 printing students by cohort, iteration over array
-  array = []
-    puts "Enter cohort to print"
-      user_input = gets.chop.downcase.to_sym
-      $students.map {|student| array << student if student[:cohort] == user_input}
-      array.each {|student| puts "#{student[:name]} (#{student[:cohort]})"}
-    end
+interactive_menu
 
-def option_menu(students) # Added a menu for better usability
-  puts "  "
-  puts "1.)......To make any CHANGES, ENTER 'c'"
-  puts "2.)......To SEARCH students by cohort ENTER 's'"
-  puts "3.)......To FINISH press ENTER"
-  choice = gets.chop.downcase
-    if choice == "c"
-      puts "Enter the student number you want to change"
-        student_num = gets.chop.to_i
-      puts "Enter the correct name"
-        new_name = gets.chop.to_sym
-      puts "Enter the correct cohort"
-        new_cohort = gets.chop.to_sym
-        index = (student_num - 1)
-        $students[index][:name] = new_name
-        $students[index][:cohort] = new_cohort
-      end
-    if choice == "s"
-        print_student_by_cohort(students)
-    else
-        print(students)
-    end
-end
-students = input_students
-print_header
-print(students)
-print_footer(students)
-option_menu(students)
+# def option_menu(students) # Added a menu for better usability
+#   puts "  "
+#   puts "1.)......To make any CHANGES, ENTER 'c'"
+#   puts "2.)......To SEARCH students by cohort ENTER 's'"
+#   puts "3.)......To FINISH press ENTER"
+#   choice = gets.chop.downcase
+#     if choice == "c"
+#       puts "Enter the student number you want to change"
+#         student_num = gets.chop.to_i
+#       puts "Enter the correct name"
+#         new_name = gets.chop.to_sym
+#       puts "Enter the correct cohort"
+#         new_cohort = gets.chop.to_sym
+#         index = (student_num - 1)
+#         $students[index][:name] = new_name
+#         $students[index][:cohort] = new_cohort
+#       end
+#     if choice == "s"
+#         print_student_by_cohort(students)
+#     else
+#         print(students)
+#     end
+# end
+# @student = input_students
+# show_students
+# print_header
+# print_student_list
+# print_footer(student)
+# # option_menu(students)
