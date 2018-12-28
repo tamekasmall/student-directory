b584e62033c2cb200e7756a0e26de9788fa9b2ad
Tameka <tamekasmall@hotmail.com>
2018-12-28 20:14:19 +0000

    Exercise 13: Taking argumentsfrom command line

--- directory.rb
+++ directory.rb
@@ -1,38 +1,5 @@
 @students = []
-def input_students
-  puts "Please enter the name of the student"
-  name = gets.chop
-    if name == ""
-    name = "name: not entered"
-    end
-  puts "Please enter cohort"
-  cohort = gets.chop
-    if cohort == ""
-    cohort = "not entered: "
-    end
-  puts "Please enter country of origin"
-  country_origin = gets.chop
-  puts "Please enter height (eg '175' for 175cm)"
-  height = gets.chop
-  puts "Please enter your favourite sport"
-  sport = gets.chop
-  while !name.empty? do
-    @students << {name: name.to_sym, cohort: cohort.to_sym, country_origin: country_origin, height: height, sport: sport}
-      if @students.length < 2
-        puts "Now we have #{@students.count} student"
-          break
-      elsif @students.length >= 2
-        puts "Now we have #{@students.count} students"
-          name = gets.chop
-      end
-  end
-end
-def interactive_menu
-  loop do
-   print_menu
-    process(gets.chomp)
-  end
-end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -40,11 +7,14 @@ def print_menu
   puts "4. Load the list from student.csv"
   puts "9. Exit"
 end
-def show_students
-  print_header
-  print_student_list
-  print_footer
+
+def interactive_menu
+  loop do
+   print_menu
+    process(STDIN.gets.chomp)
+  end
 end
+
 def process(selection)
     case selection
      when "1"
@@ -62,10 +32,46 @@ def process(selection)
    end
 end
 
+def input_students
+  puts "Please enter the name of the student"
+  name = STDIN.gets.chomp
+    if name == ""
+    name = "name: not entered"
+    end
+  puts "Please enter cohort"
+  cohort = STDIN.gets.chomp
+    if cohort == ""
+    cohort = "not entered: "
+    end
+  puts "Please enter country of origin"
+  country_origin = STDIN.gets.chomp
+  puts "Please enter height (eg '175' for 175cm)"
+  height = STDIN.gets.chomp
+  puts "Please enter your favourite sport"
+  sport = STDIN.gets.chomp
+  while !name.empty? do
+    @students << {name: name.to_sym, cohort: cohort.to_sym, country_origin: country_origin, height: height, sport: sport}
+      if @students.length < 2
+        puts "Now we have #{@students.count} student"
+          break
+      elsif @students.length >= 2
+        puts "Now we have #{@students.count} students"
+          name = STDIN.gets.chomp
+      end
+  end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
 def print_header
   puts "The students of my cohort at Makers Academy".center(20)
   puts "--------------------------------".center(45)
 end
+
 def print_student_list
   index = 0
     while index < @students.length
@@ -75,6 +81,7 @@ def print_student_list
       end
    end
 end
+
 def print_footer #8.9 pluralisation for student/s
   if @students.length == 1
     puts "Overall, we have #{@students.count} great student"
@@ -82,6 +89,7 @@ def print_footer #8.9 pluralisation for student/s
     puts "Overall, we have #{@students.count} great students"
   end
 end
+
 def save_students
   file = File.open(".student.csv", "w")
   @students.each do |student|
@@ -91,14 +99,29 @@ def save_students
   end
   file.close
 end
-def load_students
-  file = File.open(".student.csv", "r")
+
+def load_students(filename = ".student.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     @students  << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
 
 # def option_menu(students) # Added a menu for better usability
